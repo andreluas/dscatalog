ea0ec6049cf7bc4897743a5bc0efc93ee77e0821
andreluas <andreluas.k@gmail.com>
2022-11-05 18:46:44 -0300

    chore: finish x-small layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
-import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
+import Home from 'pages/Home';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <h1>Hello DSCatalog</h1>
+      <Home />
     </>
   );
 }
