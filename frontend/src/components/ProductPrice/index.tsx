c1776ad2ac86f7c89091707f0f2bbad9aa32756c
andreluas <andreluas.k@gmail.com>
2022-11-06 19:51:24 -0300

    chore: productcard, producprice and catalog page

---
+++ frontend/src/components/ProductPrice/index.tsx
@@ -0,0 +1,14 @@
+import './styles.scss';
+
+function ProductPrice() {
+  return (
+    <>
+      <div className="product-price-container">
+        <span>R$</span>
+        <h3>2345.69</h3>
+      </div>
+    </>
+  );
+}
+
+export default ProductPrice;
