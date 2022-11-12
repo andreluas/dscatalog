fe087b1d2076db7c6d0da2803ef88f4331de038a
andreluas <andreluas.k@gmail.com>
2022-11-12 14:01:53 -0300

    chore: catalog and productDetails links

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,15 +1,18 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 function ProductDetails() {
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
-        <div className="goback-container">
-          <ArrowIcon />
-          <h2>VOLTAR</h2>
-        </div>
+        <Link to="/">
+          <div className="goback-container">
+            <ArrowIcon />
+            <h2>VOLTAR</h2>
+          </div>
+        </Link>
         <div className="row">
           <div className="col-xl-6">
             <div className="img-container">
@@ -26,7 +29,10 @@ function ProductDetails() {
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do produto</h2>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, quidem.</p>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Expedita, quidem.
+              </p>
             </div>
           </div>
         </div>
